aaf9020430d4698a93cd7dc6eb19e8cda5ae0abf
VladimirOvhchinnikov <vovkaooo22@gmail.com>
2022-12-12 20:45:16 +0300

    фиксирую прибыль

--- DemoTrade/ConsoleInformation.cs
+++ DemoTrade/ConsoleInformation.cs
@@ -78,7 +78,7 @@ namespace DemoTrade
                     }
                     if (redPosition == 1)
                     {
-                        //
+                        // sign
                     }
                 }
 
@@ -93,6 +93,125 @@ namespace DemoTrade
         private void outputRegistrWindow() 
         {
             Console.Clear();
+
+            string
+                login,
+                password,
+                name,
+                surName;
+
+            Point pointArrowLogin = new Point(0, 0);
+            Point pointLogin = new Point(10, 0);
+            Point inputLogin = new Point(13,0);
+
+            Point pointArrowPassword = new Point(0, 1);
+            Point pointPassword = new Point(10, 1);
+            Point inputPassword = new Point(13, 1);
+
+            Point pointArrowName = new Point(0, 2);
+            Point pointName = new Point(10, 2);
+            Point inputName = new Point(13, 2);
+
+            Point pointArrowSurname = new Point(0, 3);
+            Point pointSurname = new Point(10, 3);
+            Point inputSurname = new Point(13, 3);
+
+            ConsoleColor colorLogin = ConsoleColor.Yellow;
+            ConsoleColor colorPassword = ConsoleColor.Yellow;
+            ConsoleColor colorName = ConsoleColor.Yellow;
+            ConsoleColor colorSurname = ConsoleColor.Yellow;
+
+            ConsoleKeyInfo keyInfo;
+            int numberPosition =0;
+            while (true)
+            {
+                outputString("Login    -> ", colorLogin, pointLogin);
+                outputString(arrow, ConsoleColor.Yellow, pointArrowLogin);
+
+                outputString("Password -> ", colorPassword, pointPassword);
+                outputString(arrow, ConsoleColor.Yellow, pointArrowPassword);
+
+                outputString("Name     -> ", colorName, pointName);
+                outputString(arrow, ConsoleColor.Yellow, pointArrowName);
+
+                outputString("Surname  -> ", colorSurname, pointSurname);
+                outputString(arrow, ConsoleColor.Yellow, pointArrowSurname);
+
+                keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    if (numberPosition == 3)
+                    {
+                        numberPosition = 0;
+                    }
+                    else numberPosition++;
+                }
+                 
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    if (numberPosition == 0)
+                    {
+                        numberPosition = 3;
+                    }
+                    else numberPosition--;
+                } 
+
+                if(keyInfo.Key == ConsoleKey.Enter) 
+                {
+                    if(numberPosition == 0) 
+                    {
+                        Console.SetCursorPosition(inputLogin.coordinateX+9, inputLogin.coordinateY);
+                        login = Console.ReadLine();
+                    }
+                    if (numberPosition == 1)
+                    {
+                        Console.SetCursorPosition(inputPassword.coordinateX + 9, inputPassword.coordinateY);
+                        password = Console.ReadLine();
+                    }
+                    if (numberPosition == 2)
+                    {
+                        Console.SetCursorPosition(inputName.coordinateX + 9, inputName.coordinateY);
+                        name = Console.ReadLine();
+                    }
+                    if (numberPosition == 3)
+                    {
+                        Console.SetCursorPosition(inputSurname.coordinateX + 9, inputSurname.coordinateY);
+                        surName = Console.ReadLine();
+                    }
+                }
+
+                if(numberPosition == 0)
+                {
+                    colorLogin = ConsoleColor.Gray;
+                    colorPassword = ConsoleColor.Yellow;
+                    colorName = ConsoleColor.Yellow;
+                    colorSurname = ConsoleColor.Yellow;
+                    //Console.SetCursorPosition(inputLogin.coordinateX+9, inputLogin.coordinateY);
+                    // string t = Console.ReadLine();
+                }
+                if (numberPosition == 1)
+                {
+                    colorLogin = ConsoleColor.Yellow;
+                    colorPassword = ConsoleColor.Gray;
+                    colorName = ConsoleColor.Yellow;
+                    colorSurname = ConsoleColor.Yellow;
+                }
+                if (numberPosition == 2)
+                {
+                    colorLogin = ConsoleColor.Yellow;
+                    colorPassword = ConsoleColor.Yellow;
+                    colorName = ConsoleColor.Gray;
+                    colorSurname = ConsoleColor.Yellow;
+                }
+                if (numberPosition == 3)
+                {
+                    colorLogin = ConsoleColor.Yellow;
+                    colorPassword = ConsoleColor.Yellow;
+                    colorName = ConsoleColor.Yellow;
+                    colorSurname = ConsoleColor.Gray;
+                }
+            }
         }
 
 
